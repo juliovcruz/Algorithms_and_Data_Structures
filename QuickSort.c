f7ff54dd2ebed22f93e980e731e1f0a361185b03
Júlio César Vieira Cruz <52674581+juliovcruz@users.noreply.github.com>
2019-11-11 14:31:51 -0300

    QuickSort

---
+++ QuickSort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void troca (int *vetor, int i, int j) {
+	int aux = vetor[i];
+	vetor[i] = vetor[j];
+	vetor[j] = aux;
+}
+
+int partition (int *vetor, int inicio, int fim) {
+	int j;
+	int pivot = vetor[fim];
+	int i = inicio - 1;
+
+	for(j = inicio; j <= fim - 1; j++) {
+		if(vetor[j] < pivot) {
+			i = i + 1;
+			troca(vetor, i, j);
+		}
+	}
+	troca(vetor, i+1, fim);
+	return (i+1);
+}
+
+void quickSort(int *vetor, int inicio, int fim) {
+	int meio;
+
+	if (inicio < fim) {
+		meio = partition (vetor, inicio, fim);
+		quickSort(vetor,inicio, meio - 1);
+		quickSort(vetor,meio+1,fim);
+	}
+}
+
+int main () {
+	int n,i;
+  
+  srand(time(NULL));
+
+	int *vetor = (int*) malloc (n * sizeof(int));
+
+	for(i = 0; i < n; i++) vetor[i] = rand()%101;
+
+	quickSort(vetor, 0, n-1);
+
+	for(i = 0; i < n; i++) printf("%d ", vetor[i]);
+
+	free (vetor);
+
+	return 0;
+}
